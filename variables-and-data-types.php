fbe4020e45d4fce173a0676f6e2ad782b7f025d1
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-07-10 01:51:18 +0600

    Variables and data types ✈  🚒

---
+++ variables-and-data-types.php
@@ -0,0 +1,12 @@
+<?php
+$name = "Jibon";
+echo "My father's name is " . $name;
+
+$name = "Hazrat Ali";
+echo "\nMy brother's name is " . $name;
+
+$myAge = 24;
+echo "\nI am " . $myAge . " years old";
+
+$amIAlive = true;
+echo "\nI am " . ($amIAlive ? "alive" : "dead");
